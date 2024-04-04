d8af5ee08167bea2eeb72957362da38badfc64ca
Griffin Riley <rileyg@oregonstate.edu>
2024-04-04 13:38:22 -0700

    solved part 2

--- solution.py
+++ solution.py
@@ -1,3 +1,6 @@
+from textwrap import fill
+
+
 directionsCoords = {
     "north": (-1,0),
     "south": (1,0),
@@ -19,6 +22,7 @@ def solvepart1():
 
     global visitedPlots
     visitedPlots = {}
+    startPos = (0,len(plotGrid)-1)
     walk(startPos, 64)
     sum = 0
 
@@ -32,20 +36,21 @@ def solvepart1():
         if ( evenCol == evenRow ): 
             sum += 1
             validPos.append(pos)
-
-            
+     
     for i in range(len(plotGrid)):
         for j in range(len(plotGrid[0])):
-            if (i,j) in visitedPlots.keys():
+            if plotGrid[i][j] == "S":
+                print("S", end="")
+            elif (i,j) in visitedPlots:
                 print("O", end="")
             else:
                 print(plotGrid[i][j], end="")
         print("")
+
     print(sum)
 
-#walk through grid and make list of visited spaces
+#walk through grid breadth-first and make list of visited spaces
 def walk(startPos, steps):
-
     queue = [(startPos, steps)]
     while len(queue) > 0:
         pos, remainingSteps = queue.pop(0)
@@ -66,6 +71,89 @@ def walk(startPos, steps):
         for dir in directionsCoords.values():
             queue.append((posAdd(pos, dir), remainingSteps-1))
 
+def solvepart2():
+    #read in data
+    data = fileRead("input.txt")
+    global plotGrid
+    plotGrid = []
+    startPos = ()
+    for i in range(len(data)):
+        plotGrid.append(list(data[i].strip()))
+        for j in range(len(plotGrid[i])):
+            if plotGrid[i][j] == "S":
+                startPos = (i,j)
+
+    #need to calculate 13 possible areas
+    #steps in area of filled map
+    #steps in area of each of 4 corner slices
+    #steps in area of each of 4 large corner slices
+    #steps in are of each of 4 points
+    allMaps = {
+        "filled": ( startPos, 201 ), 
+        "topLeftEmpty": ( (0,0), 64 ),
+        "bottomLeftEmpty": ( (len(plotGrid)-1,0), 64 ),
+        "topRightEmpty": ( (0,len(plotGrid)-1), 64 ),
+        "bottomRightEmpty": ( (len(plotGrid)-1,len(plotGrid)-1), 64 ),
+        "topLeftFull": ( (0,0), 195 ),
+        "bottomLeftFull": ( (len(plotGrid)-1,0), 195 ),
+        "topRightFull": ( (0,len(plotGrid)-1), 195 ),
+        "bottomRightFull": ( (len(plotGrid)-1,len(plotGrid)-1), 195 ),
+        "bottomPoint": ( (0, len(plotGrid) // 2), 130 ),
+        "topPoint": ( (len(plotGrid)-1, len(plotGrid) // 2), 130 ),
+        "leftPoint": ( (len(plotGrid) // 2, len(plotGrid)-1), 130 ),
+        "rightPoint": ( (len(plotGrid) // 2, 0), 130 ),
+
+    }
+    allAreas = {}
+    for name, start in allMaps.items():
+        global visitedPlots
+        visitedPlots = {}
+        walk(start[0], start[1])
+        filledAreaOdd = getEndingPointsInArea(visitedPlots.keys(), True)
+        filledAreaEven = getEndingPointsInArea(visitedPlots.keys(), False)
+        allAreas[name] = (filledAreaOdd, filledAreaEven)
+
+    #caculate how many of each area should be added
+    x = 26501365 // 131
+    areaAmounts = {
+        "filled": ( pow(x-1, 2 ), pow(x,2) ),
+        "topLeftEmpty": (0, x),
+        "bottomLeftEmpty": (0, x),
+        "topRightEmpty": (0, x),
+        "bottomRightEmpty": (0, x),
+        "topLeftFull": (x - 1, 0),
+        "bottomLeftFull": (x - 1, 0),
+        "topRightFull": (x - 1, 0),
+        "bottomRightFull": (x - 1, 0),
+        "bottomPoint": (1, 0),
+        "topPoint": (1, 0),
+        "leftPoint": (1, 0),
+        "rightPoint": (1, 0),
+
+    }
+    sum = 0
+    for k,v in areaAmounts.items():
+        for i in range(len(v)):
+            sum = sum + (v[i] * allAreas[k][i])
+
+    print(sum)
+
+
+
+#checks all spaces in a list and sees which of them are valid ending spots, given an odd number of steps
+def getEndingPointsInArea(visitedSpaces, odd):
+    area = 0
+    for pos in visitedSpaces:
+        evenCol = False
+        if ( pos[0] % 2 == 0 ): evenCol = True
+        evenRow = False
+        if ( pos[1] % 2 == 0 ): evenRow = True
+        if odd:
+            if ( evenCol != evenRow ): area += 1
+        else:
+            if ( evenCol == evenRow ): area += 1
+    return area
+
 #adds two coordinates together
 def posAdd(pos1, pos2):
     return tuple([ sum(coords) for coords in zip(pos1, pos2) ])
@@ -81,4 +169,4 @@ def fileRead(name):
         data.append(line);
     return data
 
-solvepart1()
+solvepart2()
